636eece6b1135a8b56672a0a35d9e1eea414eaa5
Sneza9 <spasic.sneska@gmail.com>
2024-11-05 17:23:04 +0100

    Add server-status, tickets and traffic component

---
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-tickets.component.html'
+})
+export class SupportTicketsComponent {
+
+}
